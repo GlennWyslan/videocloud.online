0eb926c7f5443b140d042b972eec84c3cdd97da4
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-07-21 21:21:55 -0300

    Add files via upload

---
+++ gracias-p/js/YCIPwj1300557.js
@@ -0,0 +1,13 @@
+
+      function runDelayedFunctions(data) {
+        try {
+          document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+          if(data?.setDisplayed){
+            localStorage.setItem(data?.setDisplayed, true);
+          }
+          
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
---
+++ oto1-p/js/GPRaxn1249701.js
@@ -0,0 +1,65 @@
+
+      function runDelayedFunctions(data) {
+        try {
+          document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+          if(data?.setDisplayed){
+            localStorage.setItem(data?.setDisplayed, true);
+          }
+          
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+          (function() {
+            try {
+              var vturbvideoId = "687c2376b59fc4bcfb966a49";
+              var SECONDS_TO_DISPLAY = 800;
+              var attempts = 0;
+              var elsDisplayed = false;
+              var alreadyDisplayedKey = 'alreadyElsDisplayed800';
+              var alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
+
+              var showHiddenElements = function () {
+                elsDisplayed = true;
+                runDelayedFunctions();
+                localStorage.setItem(alreadyDisplayedKey, true);
+              };
+              if (alreadyElsDisplayed === 'true') {
+                setTimeout(function () {
+                  showHiddenElements();
+                }, 100);
+              } else {
+                startWatchVideoProgress();
+              }
+              function getVideoInstance() {
+                if (smartplayer.instances.length > 1) {
+                  return smartplayer.instances.find(
+                    (instance) => instance.options.id === vturbvideoId
+                  );
+                }
+                return smartplayer.instances[0];
+              };
+              function startWatchVideoProgress() {
+                if (typeof smartplayer === 'undefined' || !(smartplayer.instances && smartplayer.instances.length)) {
+                  if (attempts >= 10) return;
+                  attempts += 1;
+                  return setTimeout(function () {
+                    startWatchVideoProgress();
+                  }, 1000);
+                }
+                console.log(smartplayer.instances);
+                var videoInstance = getVideoInstance();
+                videoInstance.on('timeupdate', () => {
+                  if (elsDisplayed || videoInstance.smartAutoPlay) return;
+                  console.log("currentTime => " +videoInstance.video.currentTime +" SECONDS_TO_DISPLAY => " +SECONDS_TO_DISPLAY);
+                  if (videoInstance.video.currentTime < SECONDS_TO_DISPLAY) return;
+                  showHiddenElements();
+                });
+              };
+            } catch (error) {
+              console.log(error);
+            }
+            
+          })();
+        

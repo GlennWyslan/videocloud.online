ca3ee71d6bcaa7d07b83dbd6a044fb78ebff14ca
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-07-28 17:09:17 -0300

    Update cbwkXc3427103.js

--- memory/js/cbwkXc3427103.js
+++ memory/js/cbwkXc3427103.js
@@ -99,10 +99,10 @@
           (function() {
             try {
               var vturbvideoId = "687c2348b59fc4bcfb966a02";
-              var SECONDS_TO_DISPLAY = 10;
+              var SECONDS_TO_DISPLAY = 954;
               var attempts = 0;
               var elsDisplayed = false;
-              var alreadyDisplayedKey = 'alreadyElsDisplayed10';
+              var alreadyDisplayedKey = 'alreadyElsDisplayed954';
               var alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
 
               var showHiddenElements = function () {
